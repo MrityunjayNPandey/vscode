53d8c317a13783ecd45c85412a093543ed7a51d4
TheWolverine796 <pandeymrityunjay796@gmail.com>
2022-02-13 20:48:35 +0530

    khj

--- CP/C_Bear_and_Prime_Numbers.cpp
+++ CP/C_Bear_and_Prime_Numbers.cpp
@@ -13,72 +13,95 @@ using namespace std;
 #define pb push_back
 #define LOG(n) 31 - __builtin_clz(n)
 #define ios ios_base::sync_with_stdio(false); cin.tie(0);
-const long long INF = 1ll << 32; const long long MAX_N = 1e6 + 7;
+const long long INF = 1ll << 32; const long long MAX_N = 1e7 + 7;
 const long long MOD = 1e9 + 7; const long long mod = 998244353;
 #define int long long
 int I;
+
+int a[10000005], qzh[1000005];
+
+int primes[10000005];
+int prime_num = 1;
+bool is_not_primes[10000005] = { false };
+
+map<int, int> mp;
+
+int read()
+{
+    int x = 0; int zf = 1; char ch = ' ';
+    while(ch != '-' && (ch < '0' || ch > '9')) ch = getchar();
+    if(ch == '-') zf = -1, ch = getchar();
+    while(ch >= '0' && ch <= '9') x = x * 10 + ch - '0', ch = getchar(); return x * zf;
+}
+
+void getPrime(int n)
+{
+    is_not_primes[0] = true;
+    is_not_primes[1] = true;
+    for(int i = 2; i <= n; i++)
+    {
+        if(is_not_primes[i] == false)
+            primes[prime_num++] = i;
+        for(int j = 1; (j < prime_num) && (primes[j] * i <= n); j++)
+        {
+            is_not_primes[primes[j] * i] = true;
+            if((i % primes[j]) == 0)
+                break;
+        }
+    }
+}
+
+
+void solve()
+{
+    getPrime(10000000);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; ++i)
+    {
+        int x;
+        cin >> x;
+        ++a[x];
+    }
+    for(int i = 1; i < prime_num; ++i)
+        for(int j = primes[i]; j <= 10000000; j += primes[i])
+            qzh[i] += a[j];
+    for(int i = 1; i < prime_num; ++i)
+        qzh[i] += qzh[i - 1];
+    int m = 0, l, r;
+    cin >> m;
+    for(int i = 1; i <= m; ++i)
+    {
+        l = 0, r = 0;
+        cin >> l >> r;
+        int ans_r = qzh[lower_bound(primes + 1, primes + prime_num, r + 1) - primes - 1];
+        int ans_l = qzh[lower_bound(primes + 1, primes + prime_num, l) - primes - 1];
+        cout << ans_r - ans_l << endl;
+    }
+}
+
+
 
 //Sieve of Eratosthenes, time complexity of O(N*log(logN))
 unordered_map<int, int> ifprime;
 vector<int> primetemp(MAX_N, 1), prime;
 void SIEVE()
 {
+    prime.pb(1);
     for(int p = 2; p <= MAX_N; p++)
     {
         if(primetemp[p] == 1)
         {
+            prime.pb(i);
             for(int i = p * p; i <= MAX_N; i += p)
             {
                 primetemp[i] = 0;
             }
         }
     }
-    prime.pb(1);
-    for(int i = 2; i <= MAX_N; i++)
-    {
-        if(primetemp[i] == 1)
-        {
-            prime.pb(i);
-            ifprime[i]++;
-        }
-    }
 }
 
-void solve()
-{
-    int n = 0, m = 0, k = 0, ans = 0;
-    cin >> n;
-    vector<int> v;
-    for(int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        v.pb(x);
-    }
-    debug(v);
-    cin >> m;
-    for(int i = 0; i < m; i++)
-    {
-        int x;
-        int y;
-        cin >> x >> y;
-        ans = 0;
-        auto p = lower_bound(prime.begin(), prime.end(), x);
-        auto q = upper_bound(prime.begin(), prime.end(), y);
-        debug(ans, x, y, *p, *q);
-        for(auto j = p; j < q; j++)
-        {
-            debug(*j);
-            int temp1 = (*j);
-            for(int k = 0; k < n; k++)
-            {
-                if(v[k] % temp1 == 0)
-                    ans++;
-            }
-        }
-        cout << ans << endl;
-    }
-}
+
 
 int32_t main()
 {
@@ -86,9 +109,9 @@ int32_t main()
 #ifdef SUBLIME
     free
 #endif
-        SIEVE();
-    int Test = 1;
-// cin >> Test;
+
+        int Test = 1;
+    // cin >> Test;
     for(I = 1; I <= Test; I++)
     {
         cerr << "-------" << I << "-------" << endl;dclear();
