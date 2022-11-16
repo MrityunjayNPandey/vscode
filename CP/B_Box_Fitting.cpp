6d882099b18ab1f7e598f6c0e94ceb8f5d3810a6
TheWolverine796 <pandeymrityunjay796@gmail.com>
2022-11-17 00:36:50 +0530

    dfdsfs

---
+++ CP/B_Box_Fitting.cpp
@@ -0,0 +1,69 @@
+/**
+ *      codeforces: _joKer_0
+ *      codechef:  joker_0000
+ *      created: 16-11-2022 18:42:07
+ **/
+// clang-format off
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#endif
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std; using namespace __gnu_pbds;
+#define endl "\n"
+#ifdef DEBUG
+#include "algo/debug.h"
+#else
+#define debug(...) 73;
+#define dclear(x) 73;
+#endif
+#define free freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);freopen("error.txt","w",stderr);
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define LOG(n) 63 - __builtin_clzll(n)
+const long long MAX_N = 1e6 + 7; const long long MOD = 1e9 + 7; const long long mod = 998244353; const long long INF = LLONG_MAX-INT_MAX;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+typedef tree<pair<int, int>, null_type, less<pair<int, int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_map;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+// clang-format on
+#define int long long
+int Test, I, tnum;
+
+void solve() {
+  int n = 0, m = 0, k = 0, ans = 1, cnt = 0, sum = 0;
+  cin >> n >> k;
+  vector<int> v(n);
+  multiset<int> mltst;
+  for (auto &i : v)
+    cin >> i, mltst.insert(i);
+  m = k;
+  debug(mltst);
+  while (mltst.size()) {
+    if ((mltst.upper_bound(m)) != mltst.begin()) {
+      debug(m);
+      auto iter = prev(mltst.upper_bound(m));
+      m -= *(iter);
+      mltst.erase(iter);
+    } else
+      ans++, m = k;
+    debug(mltst)
+  }
+  cout << ans;
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+#ifdef SUBLIME
+  free
+#endif
+      Test = 1;
+  cin >> Test;
+  for (I = 1; I <= Test; I++) {
+    dclear(I);
+    solve();
+    cout << endl;
+  }
+}
