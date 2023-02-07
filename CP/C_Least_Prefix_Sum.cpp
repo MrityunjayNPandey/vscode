f8d8ac77f26dd6bed957872788dec083128e7df1
TheWolverine796 <pandeymrityunjay796@gmail.com>
2023-02-07 15:44:07 +0530

    fsdf

--- CP/C_Least_Prefix_Sum.cpp
+++ CP/C_Least_Prefix_Sum.cpp
@@ -28,52 +28,35 @@ typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics
 typedef tree<pair<int, int>, null_type, less<pair<int, int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_map;
 typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
 // clang-format on
-// #define int long long
+#define int long long
 int Test, I, tnum;
 
 void solve() {
   int n = 0, m = 0, k = 0, ans = 0, cnt = 0, sum = 0;
   cin >> n >> m;
-  vector<int> v(n);
+  vector<int> v(n), pref(n + 1);
   for (auto &i : v)
     cin >> i;
-  vector<int> dp(n, -1);
-  function<int(int, int)> func = [&](int n1, int sum) {
-    if (n1 == n) {
-      return 0;
-    }
-    if (dp[n1] != -1)
-      return dp[n1];
-    int change = INF, nchange = INF;
-    if (v[n1] >= 0) {
-      nchange = func(n1 + 1, sum + v[n1]);
-    } else {
-      change = func(n1 + 1, sum - v[n1]) + 1;
-      if (sum + v[n1] >= 0)
-        nchange = func(n1 + 1, sum + v[n1]);
-    }
-    return min(change, nchange);
-  };
-  ans = func(m, 0);
-  debug(ans);
-  sum = 0;
-  for (int i = 0; i < m; i++)
+  priority_queue<int> pq;
+  for (int i = m - 1; i >= 1; i--) {
     sum += v[i];
-  if (v[m - 1] > 0 && m != 1) {
-    sum -= 2 * v[m - 1];
-    ans++;
+    pq.push(v[i]);
+    while (sum > 0 && pq.size()) {
+      sum -= 2 * pq.top();
+      pq.pop();
+      ans++;
+    }
   }
-  debug(sum) debug(ans);
-  int sumn = 0;
-  for (int i = 0; i < m; i++) {
-    if (v[i] < 0) {
-      if (sumn + v[i] < sum) {
-        sumn -= v[i];
-        sum -= 2 * v[i];
-        ans++;
-      }
-    } else
-      sumn += v[i];
+  debug(ans) sum = 0;
+  priority_queue<int, vector<int>, greater<>> pq1;
+  for (int i = m; i < n; i++) {
+    sum += v[i];
+    pq1.push(v[i]);
+    while (sum < 0 && pq1.size()) {
+      sum -= 2 * pq1.top();
+      pq1.pop();
+      ans++;
+    }
   }
   cout << ans;
 }
@@ -94,18 +77,3 @@ signed main() {
     cout << endl;
   }
 }
-
-/*
-Did I ever tell you what the definition of insanity is?
-Insanity is doing the exact... same fucking thing... over and over again
-expecting... shit to change... That. Is. Crazy. The first time somebody told me
-that, I dunno, I thought they were bullshitting me. The thing is... He was
-right. And then I started seeing, everywhere I looked, everywhere I looked all
-these fucking pricks, everywhere I looked, doing the exact same fucking thing...
-over and over and over and over again thinking 'this time is gonna be different'
-no, no, no please... This time is gonna be different, I'm sorry, I don't like...
-The way...
-
-Okay, Do you have a fucking problem in your head, do you think I am bullshitting
-you, do you think I am lying? Fuck you! Okay? Fuck you!...
-*/
