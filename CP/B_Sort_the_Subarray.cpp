75eaf90bf1ff894dcba49a7d27e0e13f1d7154aa
TheWolverine796 <pandeymrityunjay796@gmail.com>
2023-04-27 23:25:59 +0530

    *

---
+++ CP/B_Sort_the_Subarray.cpp
@@ -0,0 +1,81 @@
+/**
+ *      codeforces: _joKer_0
+ *      codechef:  joker_0000
+ *      created: 20-04-2023 20:19:54
+ **/
+// clang-format off
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#endif
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std; using namespace __gnu_pbds;
+#define endl "\n"
+#ifdef DEBUG
+#include "algo/debug.h"
+#else
+#define debug(...) 73;
+#define dclear(x) 73;
+#endif
+#define free freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);freopen("error.txt","w",stderr);
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define LOG(n) 63 - __builtin_clzll(n)
+const long long MAX_N = 1e6 + 7; const long long MOD = 1e9 + 7; const long long mod = 998244353; const long long INF = INT_MAX;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+typedef tree<pair<int, int>, null_type, less<pair<int, int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_map;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+// clang-format on
+#define int long long
+int Test, I, tnum;
+
+void solve() {
+  int n = 0, m = 0, k = 0, ans = 0, cnt = 0, sum = 0;
+  cin >> n;
+  vector<int> v(n), v1(n);
+  for (auto &i : v)
+    cin >> i;
+  for (auto &i : v1)
+    cin >> i;
+  int ind = -1;
+  for (int i = 0; i < n; i++) {
+    if (v[i] != v1[i]) {
+      ind = i;
+    }
+  }
+  int r = ind, l = ind;
+  for (int i = ind + 1; i < n; i++) {
+    if (v1[i] >= v1[i - 1]) {
+      r++;
+    } else {
+      break;
+    }
+  }
+  for (int i = ind - 1; i >= 0; i--) {
+    if (v1[i] <= v1[i + 1]) {
+      l--;
+    } else {
+      break;
+    }
+  }
+  cout << l + 1 << " " << r + 1;
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+#ifdef SUBLIME
+  free
+#endif
+      cout.precision(16);
+  cout << fixed;
+  Test = 1;
+  cin >> Test;
+  for (I = 1; I <= Test; I++) {
+    dclear(I);
+    solve();
+    cout << endl;
+  }
+}
---
+++ CP/C_Tear_It_Apart.cpp
@@ -0,0 +1,73 @@
+/**
+ *      codeforces: _joKer_0
+ *      codechef:  joker_0000
+ *      created: 20-04-2023 21:01:18
+ **/
+// clang-format off
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast", "unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#endif
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std; using namespace __gnu_pbds;
+#define endl "\n"
+#ifdef DEBUG
+#include "algo/debug.h"
+#else
+#define debug(...) 73;
+#define dclear(x) 73;
+#endif
+#define free freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);freopen("error.txt","w",stderr);
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define LOG(n) 63 - __builtin_clzll(n)
+const long long MAX_N = 1e6 + 7; const long long MOD = 1e9 + 7; const long long mod = 998244353; const long long INF = INT_MAX;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+typedef tree<pair<int, int>, null_type, less<pair<int, int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_map;
+typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+// clang-format on
+#define int long long
+int Test, I, tnum;
+
+void solve() {
+  int n = 0, m = 0, k = 0, ans = INF, cnt = 0, sum = 0;
+  string str;
+  cin >> str;
+  n = str.length();
+  map<char, vector<int>> mp;
+  for (int i = 0; i < n; i++) {
+    mp[str[i]].pb(i);
+  }
+  debug(mp);
+  for (auto [l, r] : mp) {
+    int st = -1, anst = 0;
+    for (int i = 0; i < r.size(); i++) {
+      if (r[i] - st - 1 > 0)
+        anst = max(anst, (int)LOG(r[i] - st - 1) + 1);
+      st = r[i];
+    }
+    if (n - 1 - st > 0)
+      anst = max(anst, (int)LOG(n - 1 - st) + 1);
+    ans = min(ans, anst);
+  }
+  cout << ans;
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+#ifdef SUBLIME
+  free
+#endif
+      cout.precision(16);
+  cout << fixed;
+  Test = 1;
+  cin >> Test;
+  for (I = 1; I <= Test; I++) {
+    dclear(I);
+    solve();
+    cout << endl;
+  }
+}
